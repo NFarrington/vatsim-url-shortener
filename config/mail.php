b833b629f42f94bfddb5032f8690a2ab3f26d77e
Neil Farrington <me@neilfarrington.com>
2018-04-14 03:59:28 -0400

    Set default mail from name/address

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@vats.im'),
+        'name' => env('MAIL_FROM_NAME', 'VATS.IM'),
     ],
 
     /*
