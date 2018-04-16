0c80ed03e0e777470126c617a8249d40ac645c50
Neil Farrington <me@neilfarrington.com>
2018-04-16 16:03:51 -0400

    Add Google Tag Manager to all pages

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -4,6 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T62XX6L');</script>
+    <!-- End Google Tag Manager -->
+
     <meta name="author" content="Neil Farrington">
     <meta name="description" content="VATS.IM is a URL shortening service for the VATSIM network.">
     <meta property="og:title" content="VATS.IM URL Shortener">
@@ -26,8 +34,12 @@
 
     @stack('meta')
 </head>
-
 <body class="text-center">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T62XX6L"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div id="app" class="cover-container d-flex p-3 mx-auto flex-column">
     <header class="masthead mb-auto">
