c4f8540b708233589a2e1e412ea8604d9673ecb2
Neil Farrington <me@neilfarrington.com>
2018-04-18 02:31:35 -0400

    Make wording on about page clearer

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
     <h1 class="cover-heading">About VATS.IM</h1>
-    <p class="lead">VATS.IM is a URL shortening service for <a href="https://vatsim.net/">VATSIM</a> and its members.</p>
-    <p>The service has been in use by VATSIM UK since 2014, providing shorter URLs that allow easy and memorable access to resources such as documents and charts, and a way of providing pilots with simple links in an Air Traffic Controller's controller or ATIS information block.</p>
-    <p>Due to the development work required to broaden the service's access, VATS.IM has been restricted to only VATSIM UK for the longest time. However, with the appropriate infrastructure now in place, the service is ready for all regions, divisions, ARTCCs, controllers, and pilots to make use of what it has to offer.</p>
+    <p class="lead">VATS.IM is a URL shortening service for <a href="https://vatsim.net/">VATSIM</a> and its members.
+    </p>
+    <p>The service has been in use by VATSIM UK since 2014, providing shorter URLs that allow easy and memorable access
+        to resources such as documents and charts, and a way of providing pilots with simple links in an Air Traffic
+        Controller's ATIS or controller information block.</p>
+    <p>Due to the development work required to broaden the service's access, VATS.IM has been restricted to only VATSIM
+        UK for the longest time. However, with the appropriate infrastructure now in place, the service is ready for all
+        regions, divisions, ARTCCs, vACCs, controllers, and pilots to make use of what it has to offer.</p>
     <p>To get started, please log in below.</p>
     <p>
         <a href="{{ route('platform.login.vatsim') }}" class="btn btn-lg btn-secondary"
