e7dd1d9fff597f445bd6d4d9b20bfc3cb496821e
Neil Farrington <me@neilfarrington.com>
2018-06-14 00:39:51 +0100

    Add managers to organization edit page

--- resources/views/platform/organizations/_form-edit.blade.php
+++ resources/views/platform/organizations/_form-edit.blade.php
@@ -53,6 +53,25 @@
 </div>
 
 <div class="form-group row">
+    <label class="col-sm-2 col-form-label">Managers</label>
+    @forelse($organization->managers as $manager)
+        <div class="{{ !$loop->first ? 'offset-sm-2' : '' }} col-sm-10">
+            <p class="form-control-plaintext">
+                {{ $manager->display_info }}
+                <delete-resource link-only
+                                 route="{{ route('platform.organizations.users.destroy', [$organization, $manager]) }}">
+                    (Remove)
+                </delete-resource>
+            </p>
+        </div>
+    @empty
+        <div class="col-sm-10">
+            <input type="text" readonly class="form-control-plaintext" value="&mdash;">
+        </div>
+    @endforelse
+</div>
+
+<div class="form-group row">
     <label class="col-sm-2 col-form-label">Members</label>
     @forelse($organization->members as $member)
         <div class="{{ !$loop->first ? 'offset-sm-2' : '' }} col-sm-10">
