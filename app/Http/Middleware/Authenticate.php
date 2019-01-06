f9118b1597b688001b7bdfe8f97afd98c8734cc3
Neil Farrington <me@neilfarrington.com>
2019-01-06 09:42:22 +0000

    Set trusted proxies via the environment

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,6 +9,7 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
+     * @codeCoverageIgnore
      * @param  \Illuminate\Http\Request $request
      * @return string
      */
---
+++ config/trustedproxy.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+     * Set trusted proxy IP addresses.
+     *
+     * Both IPv4 and IPv6 addresses are
+     * supported, along with CIDR notation.
+     *
+     * The "*" character is syntactic sugar
+     * within TrustedProxy to trust any proxy
+     * that connects directly to your server,
+     * a requirement when you cannot know the address
+     * of your proxy (e.g. if using ELB or similar).
+     *
+     */
+    'proxies' => env('TRUSTED_PROXIES', null) != '*'
+        ? explode(',', env('TRUSTED_PROXIES', null))
+        : '*',
+
+    /*
+     * Which headers to use to detect proxy related data (For, Host, Proto, Port)
+     *
+     * Options include:
+     *
+     * - Illuminate\Http\Request::HEADER_X_FORWARDED_ALL (use all x-forwarded-* headers to establish trust)
+     * - Illuminate\Http\Request::HEADER_FORWARDED (use the FORWARDED header to establish trust)
+     *
+     * @link https://symfony.com/doc/current/deployment/proxies.html
+     */
+    'headers' => Illuminate\Http\Request::HEADER_X_FORWARDED_ALL,
+
+
+];
