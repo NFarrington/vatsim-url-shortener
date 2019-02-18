4254138281488caa00535f62b03033911eca11b4
Neil Farrington <me@neilfarrington.com>
2019-02-18 20:46:18 +0000

    Merge dockerfiles

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.setPublicPath('public')
+    .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/cover.scss', 'public/css')
     .version();
