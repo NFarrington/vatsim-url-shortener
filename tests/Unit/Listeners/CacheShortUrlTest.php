b4502b0a1d6a50cc1b7c7b42a85894d2aa315fbd
Neil Farrington <me@neilfarrington.com>
2020-05-24 03:45:17 +0100

    Cache non-prefix URLs without their organization prefix

--- tests/Unit/Listeners/CacheShortUrlTest.php
+++ tests/Unit/Listeners/CacheShortUrlTest.php
@@ -13,15 +13,30 @@ class CacheShortUrlTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function caches_a_url()
+    function caches_prefixed_urls_with_a_prefix()
     {
         $url = null;
         Event::fakeFor(function () use (&$url) {
-            $url = factory(Url::class)->states('org')->create();
+            $url = factory(Url::class)->states('org')->create(['prefix' => true]);
         });
 
         event(new UrlSaved($url));
 
         $this->assertTrue(cache()->has(sprintf(Url::URL_CACHE_KEY, $url->domain->url, $url->organization->prefix, $url->url)));
+        $this->assertFalse(cache()->has(sprintf(Url::URL_CACHE_KEY, $url->domain->url, null, $url->url)));
+    }
+
+    /** @test */
+    function caches_non_prefixed_urls_without_a_prefix()
+    {
+        $url = null;
+        Event::fakeFor(function () use (&$url) {
+            $url = factory(Url::class)->states('org')->create(['prefix' => false]);
+        });
+
+        event(new UrlSaved($url));
+
+        $this->assertTrue(cache()->has(sprintf(Url::URL_CACHE_KEY, $url->domain->url, null, $url->url)));
+        $this->assertFalse(cache()->has(sprintf(Url::URL_CACHE_KEY, $url->domain->url, $url->organization->prefix, $url->url)));
     }
 }
