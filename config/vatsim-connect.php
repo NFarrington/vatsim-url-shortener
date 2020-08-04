1b00e7b1417c94195be6706817948e52ab75cc7d
Neil Farrington <me@neilfarrington.com>
2020-08-04 05:59:24 +0100

    Enable configuring Connect with Docker secrets

--- config/vatsim-connect.php
+++ config/vatsim-connect.php
@@ -3,6 +3,6 @@
 return [
     'base_url' => env('VATSIM_CONNECT_BASE_URL', 'https://auth.vatsim.net'),
     'client_id' => env('VATSIM_CONNECT_CLIENT_ID'),
-    'client_secret' => env('VATSIM_CONNECT_CLIENT_SECRET'),
+    'client_secret' => docker_secret(env('VATSIM_CONNECT_CLIENT_SECRET_SECRET')) ?: env('VATSIM_CONNECT_CLIENT_SECRET'),
     'scopes' => explode(',', env('VATSIM_CONNECT_SCOPES', 'full_name,email')),
 ];
