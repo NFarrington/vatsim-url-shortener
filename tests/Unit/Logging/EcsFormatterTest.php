ba13eb149a1a0296d266800b9c12739422fc98c3
Neil Farrington <me@neilfarrington.com>
2020-08-07 22:41:13 +0100

    Convert app logs to an ECS-like format

--- tests/Unit/Logging/EcsFormatterTest.php
+++ tests/Unit/Logging/EcsFormatterTest.php
@@ -2,30 +2,29 @@
 
 namespace Tests\Unit\Logging;
 
-use App\Logging\LogTypeCustomizer;
+use App\Logging\EcsCustomizer;
+use App\Logging\EcsFormatter;
 use Monolog\Handler\TestHandler;
 use Monolog\Logger;
 use Tests\TestCase;
 
-class LogTypeCustomizerTest extends TestCase
+class EcsFormatterTest extends TestCase
 {
-
     /** @test */
-    public function adds_a_log_type_to_logs()
+    public function adds_event_dataset_to_logs()
     {
         $testHandler = new TestHandler();
+        $testHandler->setFormatter(new EcsFormatter());
         $logger = new Logger('test-logger', [$testHandler]);
-        $customizer = new LogTypeCustomizer();
 
-        $customizer($logger);
         $logger->info('This is a test!');
 
         $records = $testHandler->getRecords();
         $this->assertCount(1, $records);
+        $jsonRecord = json_decode($records[0]['formatted'], true);
         $this->assertEquals(
-            'laravel_app',
-            $records[0]['log_type']
+            'laravel.application',
+            $jsonRecord['event']['dataset']
         );
     }
-
 }
