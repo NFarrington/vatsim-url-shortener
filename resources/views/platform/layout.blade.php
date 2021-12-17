41fc8e500e30400a8f1b30e05c443056816528e2
Neil Farrington <me@neilfarrington.com>
2021-12-17 21:14:56 +0000

    Move apple-touch-icon to /assets

--- resources/views/platform/layout.blade.php
+++ resources/views/platform/layout.blade.php
@@ -18,7 +18,7 @@
     <meta property="og:description" content="A URL shortener for VATSIM">
     <meta property="og:image" content="/assets/img/cover.jpg">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/assets/site.webmanifest">
