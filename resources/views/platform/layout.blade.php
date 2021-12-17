16e9ebcd955278088f5173daa6891b84033465c9
Neil Farrington <me@neilfarrington.com>
2021-12-17 21:18:45 +0000

    Move dimensional favicons to /assets

--- resources/views/platform/layout.blade.php
+++ resources/views/platform/layout.blade.php
@@ -19,8 +19,8 @@
     <meta property="og:image" content="/assets/img/cover.jpg">
 
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
     <link rel="manifest" href="/assets/site.webmanifest">
     <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbdd9">
     <meta name="msapplication-TileColor" content="#2d89ef">
